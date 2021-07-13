4778267b732b660b22912c026c5d9ba273605a4c
alexchea318 <alexchea319@gmail.com>
2021-07-13 23:09:01 +0300

    Add menu animation

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ var state="close";
 function open_menu(){
     var menu_selector=document.querySelector("#menu_selector");
     var menu_list=document.querySelector("#menu_list");
+    var selector_1=document.querySelector("#selector_1");
+    var selector_2=document.querySelector("#selector_2");
+    var selector_3=document.querySelector("#selector_3");
     
     if (state=="close"){
-        menu_selector.style.animation="open 0.5s ease-in-out forwards";
+        selector_1.style.animation="selector_1_open 0.5s ease-in-out forwards";
+        selector_2.style.animation="close_list 0.5s ease-in-out forwards";
+        selector_3.style.animation="selector_3_open 0.5s ease-in-out forwards";
         menu_list.style.animation="open_list 0.5s ease-in-out forwards";
         menu_list.style.display="block";
         state="open";
     } else {
-        menu_selector.style.animation="close 0.5s ease-in-out forwards";
+        selector_1.style.animation="selector_1_close 0.5s ease-in-out forwards";
+        selector_2.style.animation="open_list 0.5s ease-in-out forwards";
+        selector_3.style.animation="selector_3_close 0.5s ease-in-out forwards";
         menu_list.style.animation="close_list 0.5s ease-in-out forwards";
         state="close";
     }
