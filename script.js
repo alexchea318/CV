5b48737ce348bf2d755ebb944a237e04a346059d
alexchea318 <alexchea319@gmail.com>
2021-07-13 22:47:45 +0300

    Add menu

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+var state="close";
+
+function open_menu(){
+    var menu_selector=document.querySelector("#menu_selector");
+    var menu_list=document.querySelector("#menu_list");
+    
+    if (state=="close"){
+        menu_selector.style.animation="open 0.5s ease-in-out forwards";
+        menu_list.style.animation="open_list 0.5s ease-in-out forwards";
+        menu_list.style.display="block";
+        state="open";
+    } else {
+        menu_selector.style.animation="close 0.5s ease-in-out forwards";
+        menu_list.style.animation="close_list 0.5s ease-in-out forwards";
+        state="close";
+    }
+}
+
+
 var listener = function (event) {
     if(pageYOffset>300){
         var top_btn = document.querySelector('#to_top');
