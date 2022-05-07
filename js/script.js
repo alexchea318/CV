d76dd52455ad3a444556284440f0887f83326380
Alexander Chechenev <69586387+alexchea318@users.noreply.github.com>
2022-05-07 21:30:15 +0300

    Add experience
    And fix font bugs

--- js/script.js
+++ js/script.js
@@ -1,81 +1,105 @@
-var state="close";
-var posts;
+let state="close";
+let posts;
 
 //Mobile selector animation
-function open_menu(){
-    var menu_list=document.querySelector("#menu_list");
-    var selector_1=document.querySelector("#selector_1");
-    var selector_2=document.querySelector("#selector_2");
-    var selector_3=document.querySelector("#selector_3");
+function openMenu() {
+    const menuList=document.querySelector("#menu_list")
+    const selector1=document.querySelector("#selector_1")
+    const selector2=document.querySelector("#selector_2")
+    const selector3=document.querySelector("#selector_3")
     
     if (state=="close"){
-        selector_1.style.animation="selector_1_open 0.5s ease-in-out forwards";
-        selector_2.style.animation="close_list 0.5s ease-in-out forwards";
-        selector_3.style.animation="selector_3_open 0.5s ease-in-out forwards";
-        menu_list.style.animation="open_list 0.5s ease-in-out forwards";
-        menu_list.style.display="block";
-        state="open";
+        selector1.style.animation="selector_1_open 0.5s ease-in-out forwards"
+        selector2.style.animation="close_list 0.5s ease-in-out forwards"
+        selector3.style.animation="selector_3_open 0.5s ease-in-out forwards"
+        menuList.style.animation="open_list 0.5s ease-in-out forwards"
+        menuList.style.display="block"
+        state="open"
     } else {
-        selector_1.style.animation="selector_1_close 0.5s ease-in-out forwards";
-        selector_2.style.animation="open_list 0.5s ease-in-out forwards";
-        selector_3.style.animation="selector_3_close 0.5s ease-in-out forwards";
-        menu_list.style.animation="close_list 0.5s ease-in-out forwards";
-        state="close";
+        selector1.style.animation="selector_1_close 0.5s ease-in-out forwards"
+        selector2.style.animation="open_list 0.5s ease-in-out forwards"
+        selector3.style.animation="selector_3_close 0.5s ease-in-out forwards"
+        menuList.style.animation="close_list 0.5s ease-in-out forwards"
+        state="close"
     }
 }
 
 //Scroll to top button
 var listener = function (event) {
-    if(scrollY>300){
-        var top_btn = document.querySelector('#to_top');
-        top_btn.style.display='block';
-        top_btn.style.animation="base 0.5s";
+    const top_btn = document.querySelector('#to_top')
+    if (scrollY > 300){
+        top_btn.style.display='block'
+        top_btn.style.animation="base 0.5s"
 
+    } else {
+        top_btn.style.display='none'
+        top_btn.style.animation="base 0.5s reverse"
     }
-    else{
-        var top_btn = document.querySelector('#to_top');
-        top_btn.style.display='none'; 
-        top_btn.style.animation="base 0.5s reverse";
-    };
-};
-
-window.addEventListener("scroll", listener, false);
+}
 
 //Projects load
-function show_projects(more){
-    var project_list = document.querySelector("#project_list");
-    var template = document.querySelector('#post');
-    var total = document.querySelectorAll('.project_cell').length;
-    var final = total + more;
-    var proj=posts["projects"];
+function showProjects(more) {
+    const projectList = document.querySelector("#project_list");
+    const template = projectList.querySelector('#post');
+    let total = document.querySelectorAll('.project_cell').length;
+    const final = total + more;
+    const projects=posts["projects"];
 
-    for (var i = total; i < final; i++) {
-        total++;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("a").href=proj[i]["link"];
-        clone.querySelector("img").src="img/"+proj[i]["img"];
-        clone.querySelector("h3").textContent=proj[i]["title"];
-        clone.querySelector("p").textContent=proj[i]["text"];
-        project_list.appendChild(clone);
+    for (var i = total; i < final; i++, total++) {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector("a").href=projects[i]["link"];
+        clone.querySelector("img").src="img/"+projects[i]["img"];
+        clone.querySelector("h3").textContent=projects[i]["title"];
+        clone.querySelector("p").textContent=projects[i]["text"];
+        projectList.appendChild(clone);
 
-        if (total===posts["projects"].length){
+        if (total===posts["projects"].length - 1){
             document.querySelector("#more_btn").style.display="none";
             return;
         }
     }
 }
 
-function get_posts(){
-    var requestURL = 'js/posts.json';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        posts = request.response;
-        show_projects(2);
+//Experiance load
+function showExperience(experience) {
+    const experienceList = document.querySelector("#experience_list");
+    const template = experienceList.querySelector('#post');
+    
+    experience["experience"].map(item => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector("img").src="img/"+item["img"];
+        clone.querySelector("h3").textContent=item["title"];
+        clone.querySelector("h4").textContent=item["subtitle"];
+        clone.querySelector("time").textContent=item["time"];
+        clone.querySelector("p").textContent=item["text"];
+        experienceList.appendChild(clone);
+    });
+}
+
+function getRequest(name){
+    let requestURL = 'json/' + name
+    let request = new XMLHttpRequest()
+    request.open('GET', requestURL)
+    request.responseType = 'json'
+    request.send()
+    return request
+}
+
+//Get content from server
+function getContent(){
+    let postsRequest = getRequest("posts.json")
+    postsRequest.onload = () => {
+        posts = postsRequest.response;
+        showProjects(2);
+    }
+    
+    experienceRequest = getRequest("experience.json")
+    experienceRequest.onload = () => {
+        showExperience(experienceRequest.response);
     }
 }
 
-document.addEventListener("DOMContentLoaded", get_posts);
+//Entry points
+document.addEventListener("DOMContentLoaded", getContent);
+window.addEventListener("scroll", listener, false);
 
